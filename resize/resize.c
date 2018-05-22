f4adac66c039b43d1426837c9e6cfdaaf291d5ea
Heartsonsunday <leahgdg@hotmail.com>
2018-05-22 01:13:55 +0000

    resize ftw

--- resize/resize.c
+++ resize/resize.c
@@ -10,47 +10,61 @@ int main(int argc, char *argv[])
     // ensure proper usage
     if (argc != 4)
     {
-        fprintf(stderr, "Usage: copy multiplier infile outfile\n");
+        fprintf(stderr, "Usage: ./resize n infile outfile\n");
         return 1;
     }
-
-    //initialize resize factor
+    /*Create variable for multiplier used to change photo size*/
+    //multiplier
     int mult = atoi(argv[1]);
+
+    /*Use pointers to access location of files*/
     // remember filenames
     char *infile = argv[2];
     char *outfile = argv[3];
 
-    //printf("The Factor is: %c\n", *factor);
-    printf("The multiplier is: %i\n", mult);
-
-     if (mult < 0 || mult > 100)
-    {
-        fprintf(stderr, "Multiplier must be an integer greater than 1 and less than 100\n");
-        return 2;
-    }
-
+    ///*Use a file pointer to open the infile*/
     // open input file
     FILE *inptr = fopen(infile, "r");
     if (inptr == NULL)
     {
-        fprintf(stderr, "Could not open %s.\n", infile);
-        return 3;
+/*if it isnt opened:
+    A. the files doesnrt exist
+    B. wrong permissions,
+        fopen() allows read permission
+        to infile: line 33
+        */
+        fprintf(stderr, "Could not open %s.\nCheck spelling of infile.\n", infile);
+        return 2;
     }
 
     // open output file
     FILE *outptr = fopen(outfile, "w");
     if (outptr == NULL)
     {
+/*if it isnt opened:
+    A. the files doesnrt exist
+    B. wrong permissions,
+        fopen() allows write permission
+        to outfile: line 41
+        */
         fclose(inptr);
-        fprintf(stderr, "Could not create %s.\n", outfile);
-        return 4;
+        fprintf(stderr, "Could not create %s.\nCheck spelling of outfile.\n", outfile);
+        return 3;
     }
 
     // read infile's BITMAPFILEHEADER
+/*Plan to use BITMAPFILEHEADER to create struct nameed bf
+    Use fread() to create the struct from the infile
+    Use infile to read 1 element the size of the struct named bf and start at location of pointer &bf
+    */
     BITMAPFILEHEADER bf;
     fread(&bf, sizeof(BITMAPFILEHEADER), 1, inptr);
 
     // read infile's BITMAPINFOHEADER
+/*Plan to use BITMAPINFOHEADER on bmp.h to create struct nameed bi
+    Use fread() to create the struct from the infile
+    Use infile to read 1 element the size of the struct named bf and start at location of pointer &bf
+    */
     BITMAPINFOHEADER bi;
     fread(&bi, sizeof(BITMAPINFOHEADER), 1, inptr);
 
@@ -60,130 +74,79 @@ int main(int argc, char *argv[])
     {
         fclose(outptr);
         fclose(inptr);
-        fprintf(stderr, "Unsupported file format.\n");
-        return 5;
+        fprintf(stderr, "Unsupported file format.\nFix your pic!\n");
+        return 4;
     }
 
-    BITMAPFILEHEADER NEWbf = bf;
-    BITMAPINFOHEADER NEWbi = bi;
+/*New Structs for outfile*/
+BITMAPFILEHEADER NEWbf = bf;
+BITMAPINFOHEADER NEWbi = bi;
 
-    //int newWidth = bi.biWidth * mult;
-    //printf("new width: %i\n", newWidth);
-    NEWbi.biWidth *= mult;
+/*Variables for outfile*/
 
-    //int newHeight = bi.biHeight * mult;
-    //printf("new hidth: %i\n", newHeight);
-    NEWbi.biHeight *= mult;
-    //printf("old higth: %i\n", bi.biHeight);
-
-    // determine padding for scanlines
-    int padding = (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4)%4;
-    //printf("padding = %i\n", padding);
-
-    int NEWpadding = (4 - (NEWbi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
-    //printf("New padding = %i\n", NEWpadding);
-
-    NEWbi.biSizeImage = (sizeof(RGBTRIPLE) * NEWbi.biWidth + NEWpadding) * abs(NEWbi.biHeight);
-    //printf("new bi Size: %i\n", NEWbibiSizeImage);
-
-    NEWbf.bfSize = NEWbi.biSizeImage + sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);
-    //printf("new bf Size: %i\n", NEWbfbfSize);
+NEWbi.biWidth *= mult;
+NEWbi.biHeight *= mult;
+// determine padding for scanlines
+int padding = (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
+int NEWpadding = (4 - (NEWbi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
+NEWbi.biSizeImage = (sizeof(RGBTRIPLE) * NEWbi.biWidth + NEWpadding) * abs(NEWbi.biHeight);
+NEWbf.bfSize = NEWbi.biSizeImage + sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);
 
 
-/*
-use the newly calculated variables for biSizeImage (affects the INFOHEADER) and bfSize (affects the FILEHEADER)
-Dereference the struct!!!
-*/
-
-    // read infile's BITMAPFILEHEADER
-   // BITMAPFILEHEADER NEWbf;
-    //NEWbf.bfSize = NEWbfbfSize;
-    //printf("%i\n", NEWbfbfSize);
-    // read infile's BITMAPINFOHEADER
-   // BITMAPINFOHEADER NEWbi;
-    //bi.biSizeImage = NEWbibiSizeImage;
-    //printf("%i\n", NEWbibiSizeImage);
 
 
     // write outfile's BITMAPFILEHEADER
+/*Use the outfile to write 1 element the size BITMAPFILEHEADER starting at &bf*/
     fwrite(&NEWbf, sizeof(BITMAPFILEHEADER), 1, outptr);
 
+/*Write to the outfie 1 element the size BITMAPINFOHEADER starting from location of struct bi*/
     // write outfile's BITMAPINFOHEADER
     fwrite(&NEWbi, sizeof(BITMAPINFOHEADER), 1, outptr);
 
 
+
     // iterate over infile's scanlines
+/*start at the first pixel and read across the width writing pixels into
+    file with struct triple*/
+
     for (int i = 0, biHeight = abs(bi.biHeight); i < biHeight; i++)
     {
-        //iterate for mult times (adjusts the height)
-        for (int j = 0; j < mult; j++)
+        for (int vertical = 0; vertical < mult; vertical++)
         {
             // iterate over pixels in scanline
-            for (int p = 0; p < NEWbi.biWidth; p++)
+            for (int j = 0; j < bi.biWidth; j++)
             {
-            // temporary storage
-            RGBTRIPLE triple;
-            //RGBTRIPLE lines[sizeof(newWidth)];
-            //RGBTRIPLE newLines[sizeof(NEWbi.biWidth)];
-            //int m = 0; //always less than then newWidth
+                // temporary storage
+                RGBTRIPLE triple;
 
+                // read RGB triple from infile
+                fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
 
-            // read RGB triple from infile
-            fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
+                for (int horizontal = 0; horizontal < mult; horizontal++)
+                {
+                    // write RGB triple to outfile
+                    fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
+                }
 
-            for (int l = 0; l < mult; l++)
-            {
-                fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
-                //newLine
-                //Write to the array multiplier times
-                //newLines[ m ] = triple;
-               // printf("variable n is %i\n", n);
-                // printf("Variable m is %i\n", m);
-                // printf("Variable l is %i\n", l);
-                // printf("Variable j is %i\n", j);
-
-                // m += 1;
-                // write RGB triple to outfile
-                //fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
-                //fwrite(&triple, sizeof(RGBTRIPLE), 1, &lines);
             }
 
-            // for (int n = 0; n < mult; n++ )
-            // {
-            //     for ( int o = 0; o < sizeof(newLines); o++)
-            //     {
-            //     //fwrite(&newLines[o], sizeof(RGBTRIPLE), 1, outptr);
-            //     }
-            // }
-
-
-//Array is written
-//Write the Array to the the scanline (multiplier number of times)
-        }
-
-
-            //printf("The index of the array is: %i", m);
-
-            // skip over padding, if any
+            //skip over padding, if any
             fseek(inptr, padding, SEEK_CUR);
 
-            //add padding back in
+            //Add padding to outfile
             for (int k = 0; k < NEWpadding; k++)
             {
                 fputc(0x00, outptr);
             }
 
+            if (vertical < mult - 1)
+            {
+                fseek(inptr, -(bi.biWidth * 3 + padding), SEEK_CUR);
 
-        // skip over padding, if any
-        fseek(inptr, padding, SEEK_CUR);
-
-        // then add it back (to demonstrate how)
-        if (j < mult - 1)
-        {
-            fseek(inptr, -(bi.biWidth * 3 + padding), SEEK_CUR);
+            }
         }
+
     }
-}
 
     // close infile
     fclose(inptr);
@@ -196,42 +159,3 @@ Dereference the struct!!!
 }
 
 
-/*WHAT CHANGES
-
-bi is a struct
-Create a new strut? Name it NEW
-Create new variable to store calculated amount of padding
-
-
-NEW.biWidth = bi.biWidth *= multiplier
-
-NEW.biHeight = bi.biHeight *= multiplier
-
-Calculate new padding:
-NEW PADDING = int NEWpadding = (4 - (NEWbi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
-
-
-NEW.biSizeImage = bi.biSizeImage = (sizeof(RGBTRIPLE) * bi.Width * multiplier) + NEWpadding) * (abs(bi.Height) * multipier)
-ALTERNATIVE.biSizeImage = bi.biSizen = (sizeof(RGBTRIPLE) * (NEW.biWidth + Newpadding)) * (abs(NEW.biHeight)
-
-
-
-
-NEW.bf.bfSize = NEW.biSizeImage + sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER)
-
-I is confused
-
-
-
-
-
-you must dereference the pointer!!! Reassign the variables!!!
-
-new bi Size Image
-
-and
-
-and new bf.bfsize
-
-
-*/
